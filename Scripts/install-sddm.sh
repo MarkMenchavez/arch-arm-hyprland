01f37648bae363ba815fd6d16263edc52ae63fc1
Mark Menchavez <markchristian.menchavez@gmail.com>
2025-08-23 02:29:28 +0800

    Fix sddm script

--- Scripts/install-sddm.sh
+++ Scripts/install-sddm.sh
@@ -5,20 +5,21 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/functions.sh"
 
-yay -Sy --noconfirm --needed sddm archlinux-themes-sddm
+yay -Sy --noconfirm --needed sddm xorg-server xorg-xinit xterm archlinux-themes-sddm
 
-sudo mkdir -p /etc/sddn.conf.d
+sudo mkdir -p /etc/sddm.conf.d
 sudo tee /etc/sddm.conf.d/theme.conf >/dev/null <<EOF
 [Theme]
 Current=archlinux-simplyblack
 CursorTheme=
 EOF
 
-sudo tee /usr/share/xsessions/bash-session.desktop >/dev/null <<EOF
+sudo mkdir -p /etc/share/xsessions
+sudo tee /usr/share/xsessions/terminal-session.desktop >/dev/null <<EOF
 [Desktop Entry]
-Name=Bash
-Comment=Plain Bash Session
-Exec=/usr/bin/bash
+Name=Terminal
+Comment=Minimal Terminal  Session
+Exec=xterm
 Type=Application
 EOF
 
